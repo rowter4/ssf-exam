3c57b1f16eeda9fea9a9f4ff3ae429390705359a
rowter4 <nadzir.4@gmail.com>
2022-03-25 09:18:59 +0800

    initial commit

---
+++ purchaseOrderExam/src/main/java/sg/edu/nus/iss/purchaseOrderExam/PurchaseOrderExamApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.purchaseOrderExam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PurchaseOrderExamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PurchaseOrderExamApplication.class, args);
+	}
+
+}
---
+++ purchaseOrderExam/src/test/java/sg/edu/nus/iss/purchaseOrderExam/PurchaseOrderExamApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.purchaseOrderExam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PurchaseOrderExamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
